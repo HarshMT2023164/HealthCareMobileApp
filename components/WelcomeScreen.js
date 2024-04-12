ae7c09e6f5236ec55f65f29489675aa1a5aa6e32
nikki00011 <Nikita.Gupta@iiitb.ac.in>
2024-04-13 00:07:34 +0530

    welcome screen and login screen

---
+++ components/WelcomeScreen.js
@@ -0,0 +1,76 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { ImageBackground, SafeAreaView, StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import { Button } from 'react-native-paper'; // Importing Button component from React Native Paper
+const WelcomeScreen = () => {
+    const navigation = useNavigation();
+    const windowHeight = useWindowDimensions().height;
+
+    const handleLoginPress = () => {
+        navigation.navigate('LoginScreen');
+    };
+
+    return (
+        <SafeAreaView style={styles.safeArea}>
+            <View style={styles.container}>
+                <ImageBackground
+                    style={[styles.imageBackground, { height: windowHeight / 2.5 }]}
+                    resizeMode="cover"
+                    source={require('../assets/Healthcare_Worker.jpg')}
+                />
+                <View style={styles.contentContainer}>
+                    <Text variant="displayMedium"style={styles.title}>FIELD HEALTHCARE WORKER</Text>
+                    <Button 
+                        mode="contained-tonal" 
+                        onPress={handleLoginPress} 
+                        style={styles.loginButton}
+                        labelStyle={styles.buttonText}
+                    >
+                        Let's Get Started
+                    </Button>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        backgroundColor:'#f5f5f5',
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    imageBackground: {
+        bottom:100,
+        width: '100%',
+    },
+    contentContainer: {
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        paddingBottom: 70,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        bottom:40,
+        paddingHorizontal:40,
+        textAlign: 'center',
+    },
+    loginButton: {
+        width: '80%',
+        backgroundColor:'#e1f5fe',
+        borderColor:'black',
+        borderWidth:1,
+       
+    },
+    buttonText: {
+        fontSize: 19,
+
+    },
+});
+
+export default WelcomeScreen;
---
+++ components/login.js
@@ -0,0 +1,170 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { Button, Card, TextInput, Title } from 'react-native-paper';
+const Login = () => {
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({
+    username: '',
+    password: '',
+  });
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const authenticate = async () => {
+    try {
+      // Mocking axios post call
+      //const res = await axios.post("http://192.168.127.137:8080/auth/signin", user);
+      console.log("Authentication successful!");
+      await AsyncStorage.setItem("token","trial123");
+      console.log(await AsyncStorage.getItem("token"));
+    } catch (err) {
+      setErrorMessage("Username or password is incorrect");
+      console.log('Username or password incorrect in catch');
+    }
+  };
+
+  const handleInputChange = (name, value) => {
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleBlur = (name, value) => {
+    if (name === "username" && !value.trim()) {
+      setErrors({
+        ...errors,
+        username: "Username is required",
+      });
+    } else if (name === "password" && value.length < 6) {
+      setErrors({
+        ...errors,
+        password: "Password must be at least 6 characters",
+      });
+    } else {
+      setErrors({
+        ...errors,
+        [name]: "",
+      });
+    }
+  };
+
+  return (
+    
+    <View style={styles.container}>
+      
+      <Card mode='elevated' style={styles.card}>
+        {/* <Card.Actions>
+          <Button icon ={()=> <Image source={require('../assets/Healthcare_Worker.jpg')}  />}/>
+        </Card.Actions> */}
+     <Image source={require('../assets/Healthcare_Worker.jpg')} style={styles.logo} />
+        <Card.Content style={styles.cardContent}>
+          <Title style={styles.title}>Login</Title>
+
+          <TextInput
+            left={<TextInput.Icon icon="account" />}
+            mode='flat'
+            label='Username'
+            style={styles.input}
+            onChangeText={(value) => handleInputChange("username", value)}
+            onBlur={() => handleBlur("username", user.username)}
+            value={user.username}
+            error={errors.username ? true : false}
+          />
+          <View>{errors.username ? <Text style={styles.error}>{errors.username}</Text> : null}</View>
+          
+          <TextInput
+            left={<TextInput.Icon icon="lock" />}
+            mode='flat'
+            label='Password'
+            style={styles.input}
+            onChangeText={(value) => handleInputChange("password", value)}
+            onBlur={() => handleBlur("password", user.password)}
+            value={user.password}
+            secureTextEntry={true}
+            error={errors.password ? true : false}
+          />
+          <View>{errors.password ? <Text style={styles.error}>{errors.password}</Text> : null}</View>
+         
+        </Card.Content>
+      </Card>
+      <Button
+        mode="contained"
+        style={styles.button}
+        onPress={() => {
+          if (!errors.username && !errors.password) {
+            authenticate();
+          }
+        }}
+      >
+        <Text style={styles.buttonText}>Login</Text>
+      </Button>
+
+      {errorMessage && <Text style={styles.error}>{errorMessage}</Text>}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f8ff', // Light blue color
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center',
+    marginTop:40,
+    marginBottom: 50,
+    borderRadius: 75,// half of width and height to make it circular
+    },
+  card: {
+    width: '83%',
+    backgroundColor: 'white',
+    // marginBottom:50
+  },
+  cardContent: {
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    padding: 20,
+  },
+  title: {
+    textAlign: 'center',
+    marginBottom:10,
+    marginTop:-16,
+    fontSize: 24, // Adjust the font size
+    fontWeight: 'bold', // Apply bold font weight
+    color: '#333', // Adjust title color
+  },
+  input: {
+    marginBottom: 20,
+    backgroundColor:'#f0f8ff'
+  },
+  error: {
+    color: 'red',
+    marginBottom: 10,
+  },
+  button: {
+    width: '45%',
+    marginTop: 20,
+    backgroundColor: '#5a8de9', // Light blue color
+    borderRadius: 20,
+    marginBottom:50
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    padding: 10,
+    textAlign: 'center',
+  },
+});
+
+export default Login;
