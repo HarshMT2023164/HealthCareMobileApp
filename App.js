2dfdc611553c335eca08e38806a6826ff422c7e8
Harsh342001 <harshranpariya100@gmail.com>
2024-04-17 23:57:57 +0530

    added stack navigation in APP.js

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet, View } from "react-native";
+import "react-native-gesture-handler";
+import { MD3LightTheme as DefaultTheme, PaperProvider } from "react-native-paper";
+import { setupDatabase } from "./common/Database";
+import Questionnaire from "./components/Questionnaire";
+import Registration from "./components/Registration";
+import { useEffect } from "react";
+
+
 
 export default function App() {
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: "#008cff",
+    },
+  };
+  const Stack = createStackNavigator();
+  useEffect(() => {
+    setupDatabase();
+     // Initialize SQLite database
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Hello up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <NavigationContainer >
+          <Stack.Navigator screenOptions={{headerShown : false,}} initialRouteName="register">
+            <Stack.Screen name="register" component={Registration} />
+            <Stack.Screen name="question" component={Questionnaire} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
