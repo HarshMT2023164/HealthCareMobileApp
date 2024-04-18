58fd6cd9a53262e62d80d3e7ae644aa0172d134f
nikki00011 <Nikita.Gupta@iiitb.ac.in>
2024-04-18 15:54:06 +0530

    Profile Screen created

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import * as React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Profile from './components/Profile';
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Profile" component={Profile} options={{headerShown:false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
