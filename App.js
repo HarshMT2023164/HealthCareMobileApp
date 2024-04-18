9cbcbf12c0fb76aaf064b02044db24e4b2200088
nikki00011 <Nikita.Gupta@iiitb.ac.in>
2024-04-18 16:37:53 +0530

    Follow Up List Created

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { PaperProvider } from 'react-native-paper';
+import FollowUpListScreen from './components/followUpList';
+
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Hello up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView>
+    <PaperProvider>
+      <View style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator >
+          <Stack.Screen name="followUpList" component={FollowUpListScreen} options={{headerShown:false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+      </View>
+    </PaperProvider>
+    </GestureHandlerRootView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const styles = StyleSheet.create(
+  {
+    container:
+    {
+      flex:1,
+    }
+  }
+)
