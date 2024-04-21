1886ceb6744b4721f9c927cf92954cd5a2583ee9
Harsh342001 <harshranpariya100@gmail.com>
2024-04-21 19:12:16 +0530

    added sync in home page

--- components/Home.js
+++ components/Home.js
@@ -3,17 +3,18 @@ import { View, StyleSheet,Image, ScrollView } from 'react-native';
 import { Appbar, Text, Surface, Card, Button, TextInput } from 'react-native-paper';
 import { Picker } from '@react-native-picker/picker';
 import { useNavigation } from "@react-navigation/native";
-
+import { AntDesign } from '@expo/vector-icons';
+import { syncDataWithBackend } from '../utils/dataSyncService';
 const Home = () => {
     const [selectedValue, setSelectedValue] = useState("option2");
     const [currentDate, setCurrentDate] = useState(new Date());
     const navigation = useNavigation();
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentDate(new Date());
-        }, 60000); // Update time every minute (60 seconds)
-        return () => clearInterval(interval);
+        // const interval = setInterval(() => {
+        //     setCurrentDate(new Date());
+        // }, 60000); // Update time every minute (60 seconds)
+        // return () => clearInterval(interval);
     }, []);
 
     const formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
@@ -25,6 +26,10 @@ const Home = () => {
                 <Surface style={styles.appbarItem1}>
                     <Text style={styles.appbarItemText}>Harshil</Text>
                 </Surface>
+              
+                 
+                    
+                
                 <View style={styles.appbarItem2}>
                     <Picker
                         mode="dropdown"
@@ -32,10 +37,13 @@ const Home = () => {
                         onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
                         style={styles.appbarDropdown}
                     >
-                        <Picker.Item label="Language 1" value="Language 1" />
-                        <Picker.Item label="Language 2" value="Language 2" />
-                        <Picker.Item label="Language 3" value="Language 3" />
+                        <Picker.Item label="English" value="Language 1" />
+                        <Picker.Item label="Hindi" value="Language 2" />
+                        <Picker.Item label="Gujarati" value="Language 3" />
                     </Picker>
+                </View>
+                <View style={styles.appbarItem3}>
+                <AntDesign name="sync" size={24} color="black" onPress={() => syncDataWithBackend()}/>
                 </View>
             </Appbar.Header>
             {/* <View style={styles.dateTimeContainer}>
@@ -132,7 +140,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         marginBottom: 10,
         borderRadius: 15,
-        width: '50%', // Adjusted width for Surface
+        width: '30%', // Adjusted width for Surface
     },
     appbarItem2: {
         backgroundColor: '#FFFFFF', // White
@@ -141,7 +149,14 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         justifyContent: 'center',
         marginBottom: 10,
-        width: '45%',
+        width: '40%',
+    },
+    appbarItem3 : {
+        padding : 10,
+        width : "20%",
+        display : "flex",
+        alignItems : "center",
+
     },
     appbarItemText: {
         fontSize: 16,
