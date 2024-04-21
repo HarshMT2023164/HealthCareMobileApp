5c41e953996217632141986ec04d33d30d57a4e8
nikki00011 <Nikita.Gupta@iiitb.ac.in>
2024-04-21 16:21:51 +0530

    Pin/fingerprint Authentication done

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import WelcomeScreen from './components/WelcomeScreen';
 
+import * as LocalAuthentication from 'expo-local-authentication';
 import Login from './components/login';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(()=> {
+    async function authenticate(){
+      const result =await LocalAuthentication.authenticateAsync();
+      console.log(result);
+    }
+    authenticate();
+  },[]);
   return (
     <GestureHandlerRootView>
     <NavigationContainer>
