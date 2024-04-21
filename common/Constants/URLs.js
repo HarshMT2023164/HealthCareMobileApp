3ab339f02cc7d6bbff825b1a06b41bfda7ed42b2
Harsh342001 <harshranpariya100@gmail.com>
2024-04-21 19:11:02 +0530

    added urls const

---
+++ common/Constants/URLs.js
@@ -0,0 +1,11 @@
+export const BASE_URL = "http://192.168.195.137:8080";
+
+export const Register_Patient = "/FieldHealthCareWorker/registerCitizens";
+
+export const FETCH_QUESTIONNAIRE = "/blackBox/getQuestionnaire";
+
+export const FETCH_DOCTORLIST = "/FieldHealthCareWorker/getDoctorsByDistID";
+
+export const ADD_RESPONSES = "/FieldHealthCareWorker/addResponses";
+
+export const ASSIGN_DOCTOR = "/FieldHealthCareWorker/assignDoctorsToCitizens";
--- common/urls.js
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = "http://192.168.193.137:8082";
-
-export const Register_Patient = "/FieldHealthCareWorker/registerCitizens";
-
-export const Fetch_Questionnarie = "/blackBox/getQuestionnaire";
