8867191296653e709e1bbb6802ac7e87d8038eb0
nikki00011 <Nikita.Gupta@iiitb.ac.in>
2024-04-23 00:41:45 +0530

    "changed styling"

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
 
-import Home from './components/Home';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Icon, IconButton } from 'react-native-paper';
-import DoctorListScreen from './components/DoctorListScreen';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Icon } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import DoctorListScreen from './components/DoctorListScreen';
+import Home from './components/Home';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -16,7 +16,7 @@ export default function App() {
     <SafeAreaProvider>
       <GestureHandlerRootView>
         <NavigationContainer>
-          <Tab.Navigator initialRouteName='Home' activeColor='#37474F' inactiveColor='#A7CAE7' activeIndicatorStyle={{backgroundColor:'#BBDEFB',}} barStyle={{backgroundColor:'white'}} labeled={false}>
+          <Tab.Navigator initialRouteName='Notifications' activeColor='#37474F' inactiveColor='#A7CAE7' activeIndicatorStyle={{backgroundColor:'#BBDEFB',}} barStyle={{backgroundColor:'white'}} labeled={false}>
             <Tab.Screen name='Home' component={Home} options={{tabBarIcon:({color})=>(<Icon source='home' size={25} color={color}/>),}}/>
             <Tab.Screen name='Notifications' component={DoctorListScreen} options={{tabBarIcon:({color})=>(<Icon source='bell' size={25} color={color}/>),}}/>
             <Tab.Screen name='Profile' component={Home} options={{tabBarIcon:({color})=>(<Icon source='account-circle' size={25} color={color}/>),}}/>
--- components/DoctorListScreen.js
+++ components/DoctorListScreen.js
@@ -1,124 +1,54 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import { Chip, Text, Icon,Avatar, Surface, Button } from 'react-native-paper';
+import { Button, Divider, Surface, Text } from 'react-native-paper';
 
+// Constant data for doctors
+const doctorData = [
+  { id: 1, name: 'Dr. Tanvi Motwani',designation: 'Neurologist at kavery hospital',age:'25', gender: 'Female', phoneNumber: '9855XXXXXX' },
+  { id: 2, name: 'Dr. Rahul Singh',designation: 'Neurologist at kavery hospital',age:'25',  gender: 'Male', phoneNumber: '9855XXXXXX' },
+  { id: 3, name: 'Dr. Priya Patel',designation: 'Neurologist at kavery hospital',age:'25',  gender: 'Female', phoneNumber: '9855XXXXXX' },
+  { id: 4, name: 'Dr. Amit Kumar',designation: 'Neurologist at kavery hospital',age:'25',  gender: 'Male', phoneNumber: '9855XXXXXX' },
+  { id: 5, name: 'Dr. Neha Joshi',designation: 'Neurologist at kavery hospital',age:'25',  gender: 'Female', phoneNumber: '9855XXXXXX' },
+  { id: 6, name: 'Dr. Suresh Yadav',designation: 'Neurologist at kavery hospital',age:'25',  gender: 'Male', phoneNumber: '9855XXXXXX' },
+  { id: 7, name: 'Dr. Riya Shah',designation: 'Neurologist at kavery hospital', age:'25', gender: 'Female', phoneNumber: '9855XXXXXX' },
+  { id: 8, name: 'Dr. Vikram Singh',designation: 'Neurologist at kavery hospital', age:'25', gender: 'Male', phoneNumber: '9855XXXXXX' },
+  { id: 9, name: 'Dr. Shreya Verma',designation: 'Neurologist at kavery hospital', age:'25', gender: 'Female', phoneNumber: '9855XXXXXX' },
+  { id: 10, name: 'Dr. Aryan Sharma',designation: 'Neurologist at kavery hospital',age:'25',  gender: 'Male', phoneNumber: '9855XXXXXX' },
+];
 
 export default function DoctorListScreen() {
   return (
     <ScrollView>
-    <View style={styles.container}>
-      <View style={styles.headingContainer}>
-        <Text style={{alignSelf:'center',fontWeight:'bold',fontSize:16}} >Available Doctors</Text>
+      <View style={styles.container}>
+        <View style={styles.headingContainer}>
+          <Text style={{ alignSelf: 'center', fontWeight: 'bold', fontSize: 16 }}>Available Doctors</Text>
+        </View>
+        {doctorData.map(doctor => (
+          <Surface key={doctor.id} mode='elevated' elevation={4} style={styles.surfaceItem}>
+            <View style={styles.surfaceContent}>
+              <View style={styles.surfaceItemContent}>
+                {/* <View style={styles.surfaceItemAvatar}>
+                  <Icon source='stethoscope' size={30} color='#a7cae7' />
+                </View> */}
+                <Image source={require('../assets/images/hospitalsign.jpg')} style={styles.doctorImage} width={80} resizeMode='contain' />
+                <View style={styles.surfaceItemDetails}>
+                  
+                  <Text style={styles.doctorName}>{doctor.name}</Text>
+                  <Text style={{fontSize:11}}>{doctor.designation}</Text>
+                  <Divider style={styles.divider} />
+                  <Text style={{fontSize: 11}}>{doctor.age} |  {doctor.gender} |  {doctor.phoneNumber}</Text>
+                 
+                </View>
+              </View>
+              <View style={styles.surfaceItemButton}>
+                <Button icon='chevron-right' contentStyle={styles.buttonContent} textColor='white'>Assign</Button>
+              </View>
+            </View>
+          </Surface>
+        ))}
       </View>
-      <Surface mode='elevated' elevation={4} style={styles.surfaceItem}>
-        <View style={styles.surfaceItemContent}>
-          <View style={styles.surfaceItemAvatar}>
-            <Avatar.Icon icon='doctor' size={50} style={{backgroundColor:'#f5f5f5',borderColor:'lightgray',borderWidth:1}}/>
-          </View>
-          <View style={styles.surfaceItemDetails}>
-            <Text>Tanvi Motwani</Text>
-            <Text>Female</Text>
-            <Text>9855XXXXXX</Text>
-          </View>
-        </View>
-        <View style={styles.surfaceItemButton}>
-          <Button icon='chevron-right' mode='contained' contentStyle={{flexDirection:'row-reverse',backgroundColor:'#7b9dbd'}}>Assign</Button>
-        </View>
-      </Surface>
-      <Surface mode='elevated' elevation={4} style={styles.surfaceItem}>
-        <View style={styles.surfaceItemContent}>
-          <View style={styles.surfaceItemAvatar}>
-            <Avatar.Icon icon='doctor' size={50} style={{backgroundColor:'#f5f5f5',borderColor:'lightgray',borderWidth:1}}/>
-          </View>
-          <View style={styles.surfaceItemDetails}>
-            <Text>Tanvi Motwani</Text>
-            <Text>Female</Text>
-            <Text>9855XXXXXX</Text>
-          </View>
-        </View>
-        <View style={styles.surfaceItemButton}>
-          <Button icon='chevron-right' mode='contained' contentStyle={{flexDirection:'row-reverse',backgroundColor:'#7b9dbd'}}>Assign</Button>
-        </View>
-      </Surface>
-      <Surface mode='elevated' elevation={4} style={styles.surfaceItem}>
-        <View style={styles.surfaceItemContent}>
-          <View style={styles.surfaceItemAvatar}>
-            <Avatar.Icon icon='doctor' size={50} style={{backgroundColor:'#f5f5f5',borderColor:'lightgray',borderWidth:1}}/>
-          </View>
-          <View style={styles.surfaceItemDetails}>
-            <Text>Tanvi Motwani</Text>
-            <Text>Female</Text>
-            <Text>9855XXXXXX</Text>
-          </View>
-        </View>
-        <View style={styles.surfaceItemButton}>
-          <Button icon='chevron-right' mode='contained' contentStyle={{flexDirection:'row-reverse',backgroundColor:'#7b9dbd'}}>Assign</Button>
-        </View>
-      </Surface>
-      <Surface mode='elevated' elevation={4} style={styles.surfaceItem}>
-        <View style={styles.surfaceItemContent}>
-          <View style={styles.surfaceItemAvatar}>
-            <Avatar.Icon icon='doctor' size={50} style={{backgroundColor:'#f5f5f5',borderColor:'lightgray',borderWidth:1}}/>
-          </View>
-          <View style={styles.surfaceItemDetails}>
-            <Text>Tanvi Motwani</Text>
-            <Text>Female</Text>
-            <Text>9855XXXXXX</Text>
-          </View>
-        </View>
-        <View style={styles.surfaceItemButton}>
-          <Button icon='chevron-right' mode='contained' contentStyle={{flexDirection:'row-reverse',backgroundColor:'#7b9dbd'}}>Assign</Button>
-        </View>
-      </Surface>
-      <Surface mode='elevated' elevation={4} style={styles.surfaceItem}>
-        <View style={styles.surfaceItemContent}>
-          <View style={styles.surfaceItemAvatar}>
-            <Avatar.Icon icon='doctor' size={50} style={{backgroundColor:'#f5f5f5',borderColor:'lightgray',borderWidth:1}}/>
-          </View>
-          <View style={styles.surfaceItemDetails}>
-            <Text>Tanvi Motwani</Text>
-            <Text>Female</Text>
-            <Text>9855XXXXXX</Text>
-          </View>
-        </View>
-        <View style={styles.surfaceItemButton}>
-          <Button icon='chevron-right' mode='contained' contentStyle={{flexDirection:'row-reverse',backgroundColor:'#7b9dbd'}}>Assign</Button>
-        </View>
-      </Surface>
-      <Surface mode='elevated' elevation={4} style={styles.surfaceItem}>
-        <View style={styles.surfaceItemContent}>
-          <View style={styles.surfaceItemAvatar}>
-            <Avatar.Icon icon='doctor' size={50} style={{backgroundColor:'#f5f5f5',borderColor:'lightgray',borderWidth:1}}/>
-          </View>
-          <View style={styles.surfaceItemDetails}>
-            <Text>Tanvi Motwani</Text>
-            <Text>Female</Text>
-            <Text>9855XXXXXX</Text>
-          </View>
-        </View>
-        <View style={styles.surfaceItemButton}>
-          <Button icon='chevron-right' mode='contained' contentStyle={{flexDirection:'row-reverse',backgroundColor:'#7b9dbd'}}>Assign</Button>
-        </View>
-      </Surface>
-      <Surface mode='elevated' elevation={4} style={styles.surfaceItem}>
-        <View style={styles.surfaceItemContent}>
-          <View style={styles.surfaceItemAvatar}>
-            <Avatar.Icon icon='doctor' size={50} style={{backgroundColor:'#f5f5f5',borderColor:'lightgray',borderWidth:1}}/>
-          </View>
-          <View style={styles.surfaceItemDetails}>
-            <Text>Tanvi Motwani</Text>
-            <Text>Female</Text>
-            <Text>9855XXXXXX</Text>
-          </View>
-        </View>
-        <View style={styles.surfaceItemButton}>
-          <Button icon='chevron-right' mode='contained' contentStyle={{flexDirection:'row-reverse',backgroundColor:'#7b9dbd'}}>Assign</Button>
-        </View>
-      </Surface>
-    </View>
     </ScrollView>
-
   );
 }
 
@@ -132,36 +62,74 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 15,
   },
+  doctorImage: {
+    width:65,
+    height: 65,
+    marginRight: 55,
+  },
   container: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#f5f5f5',
-    paddingTop: 40,
-    paddingHorizontal: 20, 
-    gap:10,
+    paddingTop: 35,
+    paddingHorizontal: 10,
+    
   },
   surfaceItem: {
     backgroundColor: 'white',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 10, 
-    width: '100%', 
-    borderRadius: 10, 
+    borderRadius: 10,
     marginBottom: 10,
     elevation: 4,
+    width: '90%',
+  },
+  surfaceContent: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    gap:10,
+    width: '100%',
   },
   surfaceItemContent: {
     flexDirection: 'row',
     alignItems: 'center',
+    gap:1,
+    
   },
   surfaceItemAvatar: {
-    marginRight: 10,
+    marginRight: 20,
+    marginTop: 15,
+  },
+  surfaceItemDetails: {
+    flex: 1,
+    marginLeft:-50,
+  },
+  doctorName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  divider: {
+    marginVertical: 5,
+    width: '100%',
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  detailsText: {
+    
   },
   surfaceItemButton: {
-    marginLeft: 'auto',
+    alignSelf: 'center',
+    marginTop: 10,
+    width:'90%',
+    
+  },
+  buttonContent: {
+    flexDirection: 'row-reverse',
+    backgroundColor: '#a7cae7',
+    height:40
   },
 });
-
